da6dd553b311c760017a05dd7bcc2a5aceb03aad
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-08 15:00:52 +0300

    udate

--- tests/test_console1.py
+++ tests/test_console1.py
@@ -2,8 +2,15 @@
 """
 Unittest for the console
 """
-import unittest
 
+
+import unittest
 import console
+
+
 class TestConsole(unittest.TestCase):
     """ testing """
+    # def __init__(self, methodName: str = "runTest") -> None:
+    #     super().__init__(methodName)
+    def __init__():
+        pass
