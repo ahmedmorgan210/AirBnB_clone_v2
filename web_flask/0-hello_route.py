73d4fb50e1847c67531ee8ed185951f01e9282a3
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-20 05:46:25 +0300

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,7 @@
 """starts a Flask web application"""
 from flask import Flask
 
+
 app = Flask(__name__, strict_slashes=False)
 
 @app.route('/')
@@ -9,5 +10,5 @@ def hello():
     """rout to hello"""
     return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
